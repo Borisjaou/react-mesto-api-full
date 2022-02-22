0b4155306927925da1d10312daf7743e750c5b11
Boris Kim <366793@gmail.com>
2022-02-23 02:18:52 +0900

    feature/logout

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -117,7 +117,7 @@ const login = (req, res, next) => {
 
 const logout = (req, res, next) => {
   console.log(req, res);
-  res.send('lolol'.toJSON());
+  res.send(res.toJSON());
   /*  res
      .clearCookie('jwt')
      // .cookie('jwt', '')
